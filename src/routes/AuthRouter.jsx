099b3ed8075162ddec6ccc6ad80663c327c13325
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-10 09:24:39 -0400

    feat: capturing started sessions

--- src/routes/AuthRouter.jsx
+++ src/routes/AuthRouter.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import { firebase } from "../firebase/config";
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
 
 const AuthRouter = () => {
+  const dispatch = useDispatch();
+
+  // notify if the status change of the authentication
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      dispatch(login(user.uid, user.displayName));
+    });
+  },[dispatch]);
+
   return (
     <Router>
       <Switch>
