f2b308b81a71a11458ccec9f369c4bb0c903891b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-13 14:56:13 -0400

    feat: new payroll reducer

---
+++ src/reducers/nominaReducer.js
@@ -0,0 +1,15 @@
+/*
+{
+    id:"ajjdjajajjajaajaka",
+    fecha:"1/1/2000",
+    pago:300.00
+}
+*/
+
+
+export const nominaReducer = (state, action) => {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  };
