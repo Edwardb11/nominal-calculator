4a94e95866777a663ac0e825d06ca02530d9f1a0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-14 11:42:15 -0400

    feat:Read the data in redux

--- src/actions/nomina.js
+++ src/actions/nomina.js
@@ -7,6 +7,7 @@
 */
 
 import { db } from "../firebase/config";
+import { types } from "../types/types";
 
 export const createRegister = (pago) => {
   return async (distpatch, getState) => {
@@ -20,5 +21,12 @@ export const createRegister = (pago) => {
 
     const ref = await db.collection(`${uid}/nominas/nomina`).add(datos);
     console.log(ref);
+  };
+};
+
+export const readRegister = (data) => {
+  return {
+    type: types.nominaRead,
+    payload: data,
   };
 };
--- src/reducers/nominaReducer.js
+++ src/reducers/nominaReducer.js
@@ -4,6 +4,11 @@ export const nominaReducer = (state = {}, action) => {
   switch (action.type) {
     case types.nominaAdd:
       return {};
+    case types.nominaRead:
+      return {
+        ...state,
+        data: action.payload,
+      };
     default:
       return state;
   }
