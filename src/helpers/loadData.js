ed4c234a7ab79f823b33ab85f7ff7bec3d210d86
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-06-14 11:13:56 -0400

    feat: reading valid information from firestore and formatting in an array

--- src/helpers/loadData.js
+++ src/helpers/loadData.js
@@ -2,7 +2,12 @@ import { db } from "../firebase/config";
 
 export const loadData = async (uid) => {
   const response = await db.collection(`${uid}/nominas/nomina`).get();
-  const data =[]
-  console.log(response)
-  return data
+  const data = [];
+  //   metodo de firebase
+  response.forEach((nomina) => {
+    const nominaData = nomina.data();
+    data.push({ id: nomina.id, ...nominaData });
+  });
+  console.log(data)
+  return data;
 };
